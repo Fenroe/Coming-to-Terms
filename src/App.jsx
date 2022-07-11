3c376bfaabc15bba1140785523b27f71b0173f3d
Fenroe <scrambles9494@gmail.com>
2022-07-11 12:53:10 +0300

    changed router from hash to browser and added redirect file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import {
   Home, About, Archive, EditPost, DeletePost, Login, Signup, Profile,
   Logout, NewPost, ProfileSettings, Preview, Post, AuthOnly, ContributorOnly,
