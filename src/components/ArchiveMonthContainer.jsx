79cb8251c7e19751704c4285566c42d18e58db9a
fenroe <scrambles9494@gmail.com>
2022-11-12 01:39:56 +0200

    fixed archive

--- src/components/ArchiveMonthContainer.jsx
+++ src/components/ArchiveMonthContainer.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { Row, Col } from './index'
+import { Row, Col, PostCard } from './index'
 
 const ArchiveMonthContainer = ({ postArray }) => {
   return (
     <Row className="gx-4 gx-lg-5 justify-content-center">
       <Col className="md-10 col-lg-8 col-xl-7">
-        <h2 className="mb-4">{postArray[0].yearAndMonthPublished}</h2>
+        <h1 className="mb-4">{postArray[0].yearAndMonthPublished}</h1>
         {postArray.map((post) =>
         <div key={post._id} className="my-2">
-          <Link to={`/posts/${post._id}`} className="archive-link">{post.title}</Link>
+          <PostCard
+          id={post.url}
+          title={post.title}
+          subtitle={post.subtitle}
+          author={post.profile.username}
+          datePublished={post.datePublishedFormatted}/>
         </div>
         )}
       </Col>
